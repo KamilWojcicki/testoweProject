7a1b2deb9e988982f4c2a35bc0e7bdd95cdf0446
Kamil Wojcicki <kamilwojcickinjr@gmail.com>
2023-04-22 21:17:20 +0200

    Initial Commit

---
+++ AppleShopProjectApp/AppleShopProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleShopProjectAppApp.swift
+//  AppleShopProjectApp
+//
+//  Created by Kamil Wójcicki on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleShopProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
